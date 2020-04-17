02680065ba22cba53a58d1f430dc996f232b362b
alexander soto <sotoalexanderbrito@hotmail.com>
2020-04-17 16:36:04 -0600

    Presentacion 1 parte 2 todo listo ya

--- ClinicaArevalo/src/Forms/Login.java
+++ ClinicaArevalo/src/Forms/Login.java
@@ -110,14 +110,15 @@ public static Usuario DatosUsuario;
             }
         });
 
-        jLabel2.setText("jLabel2");
+        jLabel2.setText("Icono 3");
 
-        jLabel4.setText("jLabel4");
+        jLabel4.setText("icono1");
 
-        jLabel5.setText("jLabel5");
+        jLabel5.setText("icono2");
 
-        pswClave.setFont(new java.awt.Font("Lucida Sans", 0, 12)); // NOI18N
-        pswClave.setForeground(new java.awt.Color(255, 255, 255));
+        pswClave.setBackground(new java.awt.Color(239, 239, 239));
+        pswClave.setFont(new java.awt.Font("Liberation Sans", 0, 14)); // NOI18N
+        pswClave.setForeground(new java.awt.Color(239, 239, 239));
         pswClave.setSelectedTextColor(new java.awt.Color(0, 0, 0));
 
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
@@ -132,9 +133,9 @@ public static Usuario DatosUsuario;
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addGap(173, 173, 173)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                            .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                        .addGap(28, 28, 28)
+                            .addComponent(jLabel5, javax.swing.GroupLayout.DEFAULT_SIZE, 53, Short.MAX_VALUE)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                         .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel2Layout.createSequentialGroup()
                                 .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
@@ -203,7 +204,7 @@ public static Usuario DatosUsuario;
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel3Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel6, javax.swing.GroupLayout.PREFERRED_SIZE, 44, Short.MAX_VALUE)
+                .addComponent(jLabel6, javax.swing.GroupLayout.DEFAULT_SIZE, 44, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
